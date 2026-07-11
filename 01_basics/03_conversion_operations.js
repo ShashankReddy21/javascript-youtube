2ac492197eecc3f183de8a1c6b7b7cd52cd49f0a
ShashankReddy21 <shashankreddympatil@gmail.com>
2026-07-11 08:27:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,34 @@
+let score = true
+
+//nsole.log(typeof score);
+//nsole.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+//  number conversion
+// "33" => 33
+// null => NaN
+// "33asds" => NaN
+// undefined => NaN
+// true => 1
+// false => 0
+
+let isabul = "shashi";
+
+let value = Boolean(isabul);
+// console.log(value);
+
+// boolean conversion
+// 1 => true;
+// "" => false
+// 0 => false
+// "shashi" => true
+
+// string converasion
+
+let num = 33;
+let val = String(num);
+console.log(val);
+console.log(typeof val);
