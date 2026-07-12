bf835acdb86e0be9c16eff58ab7d4d70c506aa41
ShashankReddy21 <shashankreddympatil@gmail.com>
2026-07-12 07:22:14 +0000

    conversion and operations done

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -30,5 +30,42 @@ let value = Boolean(isabul);
 
 let num = 33;
 let val = String(num);
-console.log(val);
-console.log(typeof val);
+// console.log(val);
+// console.log(typeof val)
+
+/********************* operations *********** */
+
+let su = 3;
+let negSu = -su;
+// console.log(negSu);
+
+// console.log(2+2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**2);
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 3);
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let x = 100;
+x++;
+console.log(x);
+
+
+
+
+/* increment link - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment  */
+
+/* ecma type conversion link -- https://tc39.es/ecma262/#sec-type-conversion */
