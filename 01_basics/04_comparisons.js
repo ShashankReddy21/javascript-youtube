98cdda3c844ed76307087c626a7be054a62a19a4
ShashankReddy21 <shashankreddympatil@gmail.com>
2026-07-12 07:41:48 +0000

    comparisons done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 >= 44);
+// console.log(2 == 3);
+// console.log(2 != 3);
+
+console.log(null > 0);  // first two = false , third = true
+console.log(null == 0);
+console.log(null >= 0); // comparisons and equality works different, in comparisons null = 0 , but in equality it is does not value
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0); // all three false
+
+/* strick check its also checks the string "===" */
+
+// ===
+
+console.log("2" === 2); // false : its checks the string
+console.log("2" == 2); // true : it does not check the string
+
+
+
+
