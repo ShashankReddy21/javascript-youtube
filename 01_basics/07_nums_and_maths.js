d01dcc3a95c721d6f6a0ac30999a56e74408f409
ShashankReddy21 <shashankreddympatil@gmail.com>
2026-07-13 09:37:35 +0000

    math and num

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,39 @@
+let num = 234
+// console.log(num);
+
+let numOne = new Number(200);
+// console.log(numOne);
+
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+
+const simple = 2367.345
+// console.log(simple.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString("en-IN"));
+// console.log(hundreds.toLocaleString());
+
+/* ++++++++++++++++++ Maths ++++++++++++++++++++++++ */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4 ,34 ,5 , 67,7 ));
+// console.log(Math.max(4, 5,6 ,6,8,9));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
